9b014449a5018fd015e804b8675296d3c95d3403
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-02-24 17:43:05 +0545

    add the app bar with color and icons

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Color myColor = Color(0xFF167AFA);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-home: Scaffold(
-  
-  appBar: AppBar(
-    
-    title: Center(child: Text('Student')),
-  ),
-
-),
-
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: myColor,
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                size: 25,
+              )),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  size: 28,
+                ))
+          ],
+          title: Center(child: Text('Student')),
+        ),
+      ),
     );
   }
 }
