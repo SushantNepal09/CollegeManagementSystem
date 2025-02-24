edf78c6c8519c7c93de4c8b96c27cfbfb1b5de02
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-02-24 23:20:38 +0545

    added navbar

--- lib/Navigationtools/navbar.dart
+++ lib/Navigationtools/navbar.dart
@@ -1,10 +1,75 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
 
 class Navbar extends StatelessWidget {
   const Navbar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    Color myColor = Color(0xFF167AFA);
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Sample Name'),
+            accountEmail: Text('Example@example.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  'https://b.fssta.com/uploads/application/soccer/headshots/713.png',
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: myColor,
+            ),
+          ),
+
+ListTile(
+  leading: Icon(Icons.supervised_user_circle_outlined),
+  title: Text('Profile'),
+  onTap:() => log('hello'),
+),
+ListTile(
+  leading: Icon(Icons.notifications_active),
+  title: Text('notifications'),
+  onTap:() => log('hello'),
+),
+
+
+Divider(),
+
+ListTile(
+  leading: Icon(Icons.lock_clock_outlined),
+  title: Text('Reset Password'),
+  onTap:() {},
+),
+ListTile(
+  leading: Icon(Icons.settings),
+  title: Text('Settings'),
+  onTap:() {},
+),
+ListTile(
+  leading: Icon(Icons.logout),
+  title: Text('Logout'),
+  onTap:() {},
+),
+Divider(),
+ListTile(
+  leading: Icon(Icons.info_rounded),
+  title: Text('About Us'),
+  onTap:() {},
+)
+
+
+        ],
+      ),
+    );
   }
 }
---
+++ lib/allvariables/counterController.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+// class AppController extends GetxController {
+//   Rx<Color> username = Color(0xFF167AFA).obs;
+//   void setColor(Color newcolor) {
+//     username.value = newcolor;
+//   }
+// }
