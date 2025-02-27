e9a4d771313f3e55f334947f94f370b18ff97831
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-02-27 10:14:35 +0545

    Color change

--- lib/allvariables/allVar.dart
+++ lib/allvariables/allVar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color primaryBlue = Color(0xFF167AFA);
-Color myGrey = Color(0xFFA9A9A9);
+const Color primaryBlue = Color(0xFF167AFA);
+const Color myGrey = Color(0xFFA9A9A9);
+const Color cardColor = Color.fromARGB(255, 255, 238, 238);
 // Color red = Colors.red;
--- lib/extravar/gridbox.dart
+++ lib/extravar/gridbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student/allvariables/allVar.dart';
 import 'package:student/allvariables/gridcontent.dart';
 
 class GridBox extends StatelessWidget {
@@ -10,51 +11,58 @@ class GridBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
-      child: Container(
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(233, 211, 207, 207),
-              borderRadius: BorderRadius.circular(25)),
-          height: MediaQuery.of(context).size.height * 0.45,
-          
-          // height: double.infinity ,
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
-            itemBuilder: (context, index) {
-              
-              return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                     boxShadow: [
-      BoxShadow(
-        // ignore: deprecated_member_use
-        color: const Color.fromARGB(93, 0, 0, 0), // Shadow color
-        spreadRadius: 2, // How much the shadow spreads
-        blurRadius: 10, // How soft the shadow looks
-        offset: Offset(4, 4), // Changes position of shadow (X, Y)
-      ),
-    ],
+      child: Material(
+        elevation: 2,
+                color: Color.fromARGB(255, 181, 224, 255),
+                borderRadius: BorderRadius.circular(25),
+        
+        child: Container(
+            height: MediaQuery.of(context).size.height * 0.45,
+        decoration: BoxDecoration(
 
-    
-                  //  gradient: LinearGradient(colors: [const Color(0xFF89f7fe),Color(0xFF66a6ff)]),
-                      borderRadius: BorderRadius.circular(14),
-                      color: const Color.fromARGB(255, 255, 238, 238),
-                    ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.network(contents[index].image,fit: BoxFit.cover,height: 100,),
-                        Text(contents[index].title),
-                      ],
+        ),
+            
+            // height: double.infinity ,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, ),
+              itemBuilder: (context, index) {
+                
+                return Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                       boxShadow: [
+        BoxShadow(
+          // ignore: deprecated_member_use
+          color: const Color.fromARGB(93, 0, 0, 0), // Shadow color
+          spreadRadius: 2, // How much the shadow spreads
+          blurRadius: 6, // How soft the shadow looks
+          offset: Offset(4, 4), // Changes position of shadow (X, Y)
+        ),
+            ],
+        
+            
+                    //  gradient: LinearGradient(colors: [const Color(0xFF89f7fe),Color(0xFF66a6ff)]),
+                        borderRadius: BorderRadius.circular(14),
+                        //  color: const Color.fromARGB(255, 255, 238, 238),
+                        color : cardColor,
+                      ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.network(contents[index].image,fit: BoxFit.cover,height: 100,),
+                          Text(contents[index].title),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-            itemCount: contents.length,
-          )),
+                );
+              },
+              itemCount: contents.length,
+            )),
+      ),
     );
   }
 }
