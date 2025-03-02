a4fb0967d83c79aaea2f6b90f7af212c3f7334bd
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-03-02 18:45:36 +0545

    commit only

--- lib/allvariables/notescontent.dart
+++ lib/allvariables/notescontent.dart
@@ -17,7 +17,7 @@ List<Notes> allNotes = //here we are creating multiple objects
   Notes(SubName: "Physics", Topic: "Thermodynamics I",Date:"2025-01-02" ,),
   Notes(SubName: "Chemistry", Topic: "Organic Chemistry II",Date: "2025-01-03"),
    Notes(SubName: "Math", Topic: "Calculas",Date: "2025-01-03"),
-    Notes(SubName: "C++", Topic: "Polymorphism",Date: "2025-01-03"),
+    Notes(SubName: "C", Topic: "Loop",Date: "2025-01-03"),
      Notes(SubName: "Microprocessor", Topic: "8085 Processor",Date: "2025-01-03"),
       Notes(SubName: "Web Technology", Topic: "Css",Date: "2025-01-03"),
 ];
