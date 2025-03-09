cf1d6ac3f6daacc7c1aef9aa6245aa1c89488e24
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-03-09 16:11:48 +0545

    second commit

---
+++ lib/allvariables/feedata.dart
@@ -0,0 +1,22 @@
+class feeDetails {
+  final String title;
+  final String cost;
+
+
+
+  feeDetails({required this.title, required this.cost});
+}
+
+List<feeDetails> allCosts =
+[
+
+feeDetails(title: "Tution Fee", cost: "25000"),
+feeDetails(title: "Transportation Fee", cost: "4500"),
+feeDetails(title: "Exam Fee", cost: "3700"),
+feeDetails(title: "Extra Fee", cost: "9900"),
+feeDetails(title: "Discount", cost: "3500"),
+feeDetails(title: "h", cost: "3500")
+
+
+
+];
--- lib/pages/fees.dart
+++ lib/pages/fees.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:student/allvariables/allVar.dart';
+import 'package:student/allvariables/feedata.dart';
 
 class Fees extends StatelessWidget {
   const Fees({super.key});
@@ -17,7 +18,8 @@ class Fees extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         )),
       ),
-      body: Column( //this is for the whole page
+      body: Column(
+        //this is for the whole page
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -30,67 +32,94 @@ class Fees extends StatelessWidget {
                   ),
                   borderRadius: BorderRadius.circular(20),
                   color: const Color.fromARGB(255, 255, 255, 255)),
+              child: Column(
+                //this is for the contents inside the first container
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20, left: 15, right: 15, bottom: 0),
+                    child: Row(
+                      // row for presenting the texts in horizontal direction
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "School Fee For April",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              letterSpacing: 1,
+                              color: const Color.fromARGB(255, 77, 77, 77)),
+                        ),
+                        Text(
+                          "07 April",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: const Color.fromARGB(255, 77, 77, 77)),
+                        ),
+                      ],
+                    ),
+                  ),
 
-child: Column( //this is for the contents inside the first container
-crossAxisAlignment: CrossAxisAlignment.start,
-children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 2, left: 15, right: 15, bottom: 10),
+                    child: Row(
+                      // row for presenting the texts in horizontal direction
 
-Padding(
-  padding: const EdgeInsets.only(top: 20,left: 15,right: 15,bottom: 0),
-    child: Row( // row for presenting the texts in horizontal direction
-    mainAxisAlignment: MainAxisAlignment.spaceBetween
-    ,
-    children: [
-  Text("School Fee For April",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 16,letterSpacing: 1,color: const Color.fromARGB(255, 77, 77, 77)),),
-  Text("07 April",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 16,color: const Color.fromARGB(255, 77, 77, 77)),),
-  
-    ],
-  ),
-),
+                      children: [
+                        Text(
+                          "Rs 25,000",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 15),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                            height: 25,
+                            width: 65,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.red),
+                            child: Center(
+                              child: Text("Pending",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12)),
+                            ))
+                      ],
+                    ),
+                  ),
 
+                  Divider(),
 
-
-Padding(
-  padding: const EdgeInsets.only(top: 2,left: 15,right: 15,bottom: 10),
-  child: Row( // row for presenting the texts in horizontal direction
-    
-  children: [
-  
-    Text("Rs 25,000",style: TextStyle(fontWeight: FontWeight.w900,fontSize: 15),),
-    SizedBox(width: 10,),
-  Container(
-    height: 25,
-    width: 65,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      color:  Colors.red
-    ),
-    child: Center(child: Text("Pending",style: TextStyle(color: Colors.white,fontSize: 12)),
-  )
-  
-  
-  )
-  ],
-  
-  
-  
-  ),
-),
-
-
-Divider(),
+                Expanded(
+                 
+                    child: ListView.builder(
+                    
+                      itemCount: allCosts.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(allCosts[index].title),
+                                Text(allCosts[index].cost),
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  )
 
 //use List view.builder
-
-
-
-
-
-],
-
-
-),
-
+                ],
+              ),
             ),
           )
         ],
