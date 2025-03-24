99813206c5c2bea7bfa9a42a0a6121d37737a0b0
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-03-24 17:02:36 +0545

    exam portion

--- lib/pages/results/result.dart
+++ lib/pages/results/result.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:student/allvariables/allVar.dart';
 
+var _dropDownValue = "CSIT";
+
 class LatestReports extends StatefulWidget {
   const LatestReports({super.key});
 
@@ -14,10 +16,74 @@ class _LatestReportsState extends State<LatestReports> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primaryBlue,
-        title: Text('Exam Results'),
-
+        title: Text(
+          'Exam Results',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: Center(child: Text('Exam Results')),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 40, bottom: 40, left: 40, right: 40),
+            child: Center(
+                child: Image.asset(
+              'assets/report.png',
+              height: 180,
+            )),
+          ),
+          Padding(
+           padding:
+                const EdgeInsets.only(top: 40, bottom: 40, left: 38, right: 38),
+            child: Container(
+              // width: MediaQuery.sizeOf(context).width * 1.1,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: .5,color: Colors.grey)
+              ),
+              child: DropdownButton(
+                  items: [
+                    DropdownMenuItem(
+                    
+                      value: 'CSIT',
+                      child: Text('  Bsc.CSIT'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'BIT',
+                      child: Text('  BIT'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'Physics',
+                      child: Text('  Bsc.Physics'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'BTECH',
+                      child: Text('  B.TECH'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'Biology',
+                      child: Text('  Biology'),
+                    ),
+                  ],
+                  value: _dropDownValue,
+                  isExpanded: true,
+
+                  borderRadius: BorderRadius.circular(10),
+                  style: TextStyle(color: const Color.fromARGB(255, 13, 13, 13),fontSize: 18,fontWeight: FontWeight.w500
+                  ),
+                  underline: Container(
+                   height: 0, 
+                    
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _dropDownValue = newValue!;
+                    });
+                  }),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
