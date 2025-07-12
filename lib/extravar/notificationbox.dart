9e557f83f2c6681de52d82d18f6b5014599d49db
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-07-12 22:28:28 +0545

    dfsdfsd

--- lib/extravar/notificationbox.dart
+++ lib/extravar/notificationbox.dart
@@ -14,7 +14,7 @@ class Square extends StatelessWidget {
         height: 50,
         width: 200,
         color: Colors.red,
-        child: Center(child: Text(child,style: TextStyle(fontSize: 31,color: Colors.red),)),
+        child: Center(child: Text(child,style: TextStyle(fontSize: 30,color: Colors.red),)),
       ),
     );
   }
