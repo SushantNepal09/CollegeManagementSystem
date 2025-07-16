11dce41865f37c67c04b0ae0fc9bb08a097b919a
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-07-16 21:46:33 +0545

    Profile pic page

--- lib/pages/Profilepage/personalinfo.dart
+++ lib/pages/Profilepage/personalinfo.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:student/allvariables/allVar.dart';
 
 class Personalinfo extends StatefulWidget {
@@ -9,29 +12,99 @@ class Personalinfo extends StatefulWidget {
 }
 
 class _PersonalinfoState extends State<Personalinfo> {
+  File ? _image;
+  pickimage() async {
+    final picker = ImagePicker();
+
+    final PickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (PickedFile != null) {
+      _image = File(PickedFile.path);
+      setState(() {
+        
+      });
+    }
+
+      // _picker.pickImage(source: ImageSource.camera);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-backgroundColor: primaryBlue,
+        appBar: AppBar(
+          backgroundColor: primaryBlue,
+          title: Text(
+            'Hello',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border.all(width: 1.5, color: Colors.blue),
+                          boxShadow: [
 
-title: Text('Hello',style: TextStyle(color: Colors.white),),
+                            BoxShadow(
+                              color: Colors.black26,
+        blurRadius: 6,
+        offset: Offset(0, 4),
+                            )
+                          ]
+,
+  gradient: LinearGradient(
+      colors: [Color(0xFF4FACFE), Color(0xFF00F2FE)],
+    ),                          
+                          
+                          ),
 
-),
 
-body: Column(
-children: [
-CircleAvatar(
-  child: ClipOval(
-child: Image.asset('assets/profile'),
+                      child: CircleAvatar(
+                        radius: 40,
+                        child: ClipOval(
+                          child: _image == null
+                              ? Image.asset('assets/profilepic.png')
+                              : Image.file(_image!,width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height*0.5,fit: BoxFit.cover,),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: GestureDetector(
+                        onTap: () {
 
-  ),
-)
-
-],
-
-)
-    
-    );
+                          pickimage();
+                        },
+                        child: Container(
+                            padding: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.white, width: 2),
+                            ),
+                            child: Icon(
+                              Icons.camera_alt_sharp,
+                              size: 20,
+                              color: Colors.white,
+                            )
+                            // child: IconButton(onPressed: () {}, icon:Icon(Icons.camera_alt_sharp,size: 20,color: Colors.white,)),
+                            ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import file_selector_macos
 import local_auth_darwin
 import open_file_mac
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
