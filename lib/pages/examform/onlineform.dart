84719c70b9b6dd2905369f0ff9e1857e68ea231a
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-08-02 18:07:19 +0545

    exam form

--- lib/pages/examform/onlineform.dart
+++ lib/pages/examform/onlineform.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:student/allvariables/allVar.dart';
 
-class Onlineform extends StatelessWidget {
+class Onlineform extends StatefulWidget {
   const Onlineform({super.key});
+
+  @override
+  State<Onlineform> createState() => _OnlineformState();
+}
+
+class _OnlineformState extends State<Onlineform> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  backgroundColor: primaryBlue,
-  title: Text('Exam form',style: TextStyle(color: Colors.white),),
-),
-body: Center(
-  child: Text('Exam Form'),
-),
+      appBar: AppBar(
+        backgroundColor: primaryBlue,
+        title: Text(
+          'Exam form',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          
+          child: Column(
+children: [
+Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: TextFormField(),
+)
+
+],
+
+        ),),
+      ),
     );
   }
 }
